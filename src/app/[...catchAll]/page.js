a9274d09762e1bb0af559d22c6faefee05e0e5b7
oghosa <oghosa07@gmail.com>
2024-09-25 02:19:24 +0100

    fix catch all

--- src/app/[...catchAll]/page.js
+++ src/app/[...catchAll]/page.js
@@ -1,11 +1,13 @@
+"use client";
+
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
-export default function CatchAll() {
+export default function CatchAllPage() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push('/');
+    router.replace('/');
   }, [router]);
 
   return null;
